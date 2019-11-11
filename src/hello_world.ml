93121757b03abdab1452cf16e586c178d99eaf3c
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-11 22:17:54 +0000

    Format Ocaml code more compactly

--- src/hello_world.ml
+++ src/hello_world.ml
@@ -1,5 +1,3 @@
 let id (x : int) = x
-
 let zero (_ : int) = 0
-
 let succ (x : int) = x + 1
--- src/hello_world.mli
+++ src/hello_world.mli
@@ -1,10 +1,9 @@
 (** Hello World
 
-    Hello world is a module I'm using to learn fun
-    Ocaml build and testing and documentation tools.
+    Hello world is a module I'm using to learn fun Ocaml build and testing and
+    documentation tools.
 
-    I've made this change to test automatic post commit hook!
-*)
+    I've made this change to test automatic post commit hook! *)
 
 val id : int -> int
 (** [id a] returns the result [a] *)
