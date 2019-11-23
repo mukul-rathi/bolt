7694f613cf85b7375b75a4b803b84a273a52b9a1
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-23 23:29:48 +0000

    Make data-race checking opt-in not opt-out
    This is for ease of testing - otherwise all tests would have to pass
    in flag "-allow-data-races"

--- src/typing/type_checker.ml
+++ src/typing/type_checker.ml
@@ -3,10 +3,10 @@ open Result
 open Type_core_lang
 open Type_data_race
 
-let type_check_program program ~allow_data_races =
+let type_check_program program ~check_data_races =
   type_core_lang program
   >>= fun typed_program ->
-  (if not allow_data_races then type_data_race typed_program else Ok ())
+  (if check_data_races then type_data_race typed_program else Ok ())
   >>| fun () -> typed_program
 
 let pprint_typed_ast ppf (prog : Typed_ast.program) = Pprint_tast.pprint_program ppf prog
--- src/typing/type_checker.mli
+++ src/typing/type_checker.mli
@@ -1,7 +1,7 @@
 open Core
 
 val type_check_program :
-  Parsed_ast.program -> allow_data_races:bool -> (Typed_ast.program, Error.t) Result.t
+  Parsed_ast.program -> check_data_races:bool -> (Typed_ast.program, Error.t) Result.t
 
 val pprint_typed_ast : Format.formatter -> Typed_ast.program -> unit
 (** Given a formatter and typed AST, pretty-print the AST - useful for debugging *)
