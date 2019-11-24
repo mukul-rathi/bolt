23b600944683461495eee8fb7d35d83f1d9b7123
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-24 00:30:25 +0000

    Add type-checking for linear reference aliasing
    Resolves #26

--- src/typing/data_race/type_linear_ownership.mli
+++ src/typing/data_race/type_linear_ownership.mli
@@ -4,7 +4,9 @@ open Core
 open Typed_ast
 open Ast_types
 
+type ownership = LinearOwned | LinearFree | NonLinear
+
 val type_linear_ownership :
-  class_defn list -> trait_defn list -> expr -> (bool, Error.t) Result.t
+  class_defn list -> trait_defn list -> expr -> (ownership, Error.t) Result.t
 (** The "Ok" result type ("") is not used at all when the function returns, it is only
     used when the function makes recursive calls to itself *)
