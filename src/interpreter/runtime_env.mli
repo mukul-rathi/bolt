91c109b5293e853ee73340e07859e769e8739280
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-25 21:36:15 +0000

    Define instructions for the interpreter
    Resolves #39

--- src/interpreter/runtime_env.mli
+++ src/interpreter/runtime_env.mli
@@ -1,4 +1,5 @@
 open Ast_types
+open Core
 
 type threadID
 (** Abstract data types for thread ID and heap address *)
@@ -8,21 +9,49 @@ type address
 val string_of_address : address -> string
 val string_of_thread_id : threadID -> string
 
-type value = NULL | ADDR of address | INT of int | FUN of (value -> expr)
+type value =
+  | NULL
+  | ADDR      of address
+  | INT       of int
+  | CLOSURE   of code * env
+  | THREAD_ID of threadID
+      (** this final value type is placed as a marker to determine the thread that the
+          current thread is waiting on *)
 
-and expr =
-  | TypedExpr of Typed_ast.expr
-  | Value     of value
-  | Blocked   of threadID
-  | Seq       of expr list
+and instruction =
+  | PUSH              of value
+  | BIND              of Var_name.t
+      (** expects the set value to be on top of the stack. Creates a new binding on stack *)
+  | BLOCKED
+  | MK_CLOSURE        of code  (** pushes a closure onto the stack *)
+  | STACK_LOOKUP      of Var_name.t
+  | STACK_SET         of Var_name.t
+      (** expects the set value to be on top of the stack. Also note - this updates the
+          most recent binding for the variable, unlike BIND which adds a new binding *)
+  | HEAP_FIELD_LOOKUP of Field_name.t
+      (** note this expects the object's Addr on top of the stack *)
+  | HEAP_FIELD_SET    of Field_name.t
+      (** note this expects [Addr, Value] as top two elements of the stack *)
+  | SWAP  (** Swaps the top two elements on the stack *)
+  | POP
+  | APPLY  (** function application - applies value to a closure *)
+  | CONSTRUCTOR       of Class_name.t  (** Creates a new object on the stack *)
+  | SPAWN             of code  (** This spawns a new thread which executes the code *)
+
+and code = instruction list
+
+and binding = Var_name.t * value
+
+and env = binding list
 
 type obj = {class_name: Class_name.t; fields: (Field_name.t * value) list}
+type env_or_value = Env of env | V of value
 
-type stack = (Var_name.t * value) list
+type stack = env_or_value list
 (** Note each thread has a local stack, but heap is global *)
 
 type heap = (address * obj) list
-type thread = TThread of threadID * expr * stack
+type thread = TThread of threadID * code * stack
 type thread_pool = thread list
 
 val init_runtime_env : Typed_ast.expr -> thread_pool * heap
