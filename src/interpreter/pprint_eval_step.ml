7861633c5b391cafd412dc12626a2bc74e34697c
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-26 11:28:49 +0000

    Adjust pprint output of interpreter
    More spacing
    
    Print items on heap in order of address
    Print thread pool with scheduled thread id first, and subsequently
    print threads in order of thread id

--- src/interpreter/pprint_eval_step.ml
+++ src/interpreter/pprint_eval_step.ml
@@ -14,13 +14,14 @@ let rec string_of_value = function
   | THREAD_ID thread_id -> Fmt.str "Thread ID: %s" (string_of_thread_id thread_id)
 
 and string_of_env env =
-  String.concat ~sep:","
+  String.concat ~sep:", "
     (List.map
        ~f:(fun (var_name, value) ->
          Fmt.str "%s -> %s" (Var_name.to_string var_name) (string_of_value value))
        env)
 
-and string_of_code code = String.concat ~sep:";" (List.map ~f:string_of_instruction code)
+and string_of_code code =
+  String.concat ~sep:"; " (List.map ~f:string_of_instruction code)
 
 and string_of_instruction = function
   | PUSH value                   -> Fmt.str "PUSH(%s)" (string_of_value value)
@@ -39,11 +40,11 @@ and string_of_instruction = function
   | APPLY                        -> "APPLY"
   | CONSTRUCTOR class_name       -> Fmt.str "CONSTRUCTOR(%s)"
                                       (Class_name.to_string class_name)
-  | SPAWN code                   -> Fmt.str "SPAWN(%s)" (string_of_code code)
+  | SPAWN code                   -> Fmt.str "SPAWN [ %s ]" (string_of_code code)
 
 let string_of_obj obj =
   let string_of_fields fields =
-    String.concat ~sep:";"
+    String.concat ~sep:"; "
       (List.map
          ~f:(fun (field_name, value) ->
            Fmt.str "%s: %s" (Field_name.to_string field_name) (string_of_value value))
@@ -53,14 +54,17 @@ let string_of_obj obj =
     (string_of_fields obj.fields)
 
 let string_of_heap heap =
-  String.concat ~sep:","
+  List.sort ~compare:(fun (addr1, _) (addr2, _) -> compare_addresses addr1 addr2) heap
+  |> fun sorted_heap ->
+  (* Print in sorted order of addresses *)
+  String.concat ~sep:", "
     (List.map
        ~f:(fun (addr, obj) ->
-         Fmt.str "%s |-> %s" (string_of_address addr) (string_of_obj obj))
-       heap)
+         Fmt.str "%s -> %s" (string_of_address addr) (string_of_obj obj))
+       sorted_heap)
 
 let string_of_stack stack =
-  String.concat ~sep:","
+  String.concat ~sep:", "
     (List.map
        ~f:(function
          | V v     -> Fmt.str "Value: %s" (string_of_value v)
@@ -73,15 +77,33 @@ let pprint_thread ppf indent (TThread (thread_id, code, stack)) =
   Fmt.pf ppf "%sInstructions: [ %s ]@." new_indent (string_of_code code) ;
   Fmt.pf ppf "%sStack: [ %s ]@." new_indent (string_of_stack stack)
 
+let pprint_thread_pool ppf indent maybe_scheduled_thread_id thread_pool =
+  List.sort
+    ~compare:(fun (TThread (tid1, _, _)) (TThread (tid2, _, _)) ->
+      compare_thread_ids tid1 tid2)
+    thread_pool
+  |> fun sorted_thread_pool ->
+  (* Print scheduled thread first *)
+  ( match maybe_scheduled_thread_id with
+  | None                     -> sorted_thread_pool
+  | Some scheduled_thread_id ->
+      ( match get_thread scheduled_thread_id thread_pool with
+      | Ok scheduled_thread -> pprint_thread ppf indent scheduled_thread
+      | Error _             -> () ) ;
+      remove_thread scheduled_thread_id thread_pool )
+  |> fun remaining_thread_pool ->
+  (* Print remaining threads in sorted order *)
+  List.iter ~f:(pprint_thread ppf indent) remaining_thread_pool
+
 let pprint_eval_step ppf ~step_number thread_pool heap maybe_scheduled_thread_id =
   let indent = "└──" in
   ( match maybe_scheduled_thread_id with
   | Some scheduled_thread_id ->
       Fmt.pf ppf "----- Step %d - scheduled thread : %s-----@." step_number
         (string_of_thread_id scheduled_thread_id)
-  | None                     -> Fmt.pf ppf "----- Step %d - OUTPUT -----@." step_number
-  ) ;
+  | None                     -> Fmt.pf ppf "----- Step %d - OUTPUT STATE --------@."
+                                  step_number ) ;
   Fmt.pf ppf "Threads:@." ;
-  List.iter ~f:(pprint_thread ppf indent) thread_pool ;
+  pprint_thread_pool ppf indent maybe_scheduled_thread_id thread_pool ;
   Fmt.pf ppf "Heap: [ %s ]@." (string_of_heap heap) ;
   Fmt.pf ppf "------------------------------------------@."
