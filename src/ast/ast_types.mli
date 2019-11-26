da4ecdf26427ee6b7dcf7ea2a9be095bf3708222
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-26 20:57:13 +0000

    Add docstrings to AST types module

--- src/ast/ast_types.mli
+++ src/ast/ast_types.mli
@@ -1,11 +1,11 @@
-(** These are the types used in constructing any AST*)
+(** These are the types used in constructing any AST for a Bolt program. Bolt programs
+    consist of a list of class definitions, followed by a list of trait definitions and
+    finally an expression to execute. *)
 
 type loc = Lexing.position
 (** Stores the line and position of the token *)
 
-val string_of_loc : loc -> string
-
-(** Distinguish between different identifiers *)
+(** An abstract type for identifiers*)
 module type ID = sig
   type t
 
@@ -14,35 +14,53 @@ module type ID = sig
   val ( = ) : t -> t -> bool
 end
 
+(** We distinguish the different identifiers for variables, classes, traits and fields. *)
+
 module Var_name : ID
 module Class_name : ID
 module Trait_name : ID
 module Field_name : ID
 
 (** Define capabilities for data references *)
-type capability = Linear | Thread | Read
+type capability =
+  | Linear  (** Only permitted one alias to the object at any time *)
+  | Thread  (** Permitted multiple aliases but only within the same thread *)
+  | Read  (** Allowed access through multiple aliases but the object must be immutable *)
 
-val string_of_cap : capability -> string
-
-(** Associate capabilities with threads *)
+(** Associate capabilities with traits *)
 type cap_trait = TCapTrait of capability * Trait_name.t
 
 (** Determines whether field is (im)mutable *)
-type mode = MConst | MVar
+type mode = MConst  (** Immutable *) | MVar  (** Mutable *)
 
-val string_of_mode : mode -> string
-
+(** Types of fields defined in classes/traits *)
 type type_field = TFieldInt
 
+(** Define types of expressions in Bolt programs*)
 type type_expr =
   | TEInt
   | TEClass    of Class_name.t
   | TECapTrait of cap_trait
   | TEFun      of type_expr * type_expr
 
-val string_of_type : type_expr -> string
-
+(** Class Field declarations are of the form "mode name : type" e.g. const f : int *)
 type field_defn = TField of mode * Field_name.t * type_field
+
+(** Trait Field declarations are similar, except they're of the form "require mode :
+    type" e.g. require const f : int *)
 type require_field_defn = TRequire of field_defn
+
+(** Class definitions consist of the class name, the trait it is implementing (with
+    capability associated) and the fields in the class *)
 type class_defn = TClass of Class_name.t * cap_trait * field_defn list
+
+(** Trait definitions consist of name and the capability and a list of the fields it
+    requires *)
 type trait_defn = TTrait of Trait_name.t * capability * require_field_defn list
+
+(** Various helper functions to convert types to equivalent string representations *)
+
+val string_of_loc : loc -> string
+val string_of_cap : capability -> string
+val string_of_mode : mode -> string
+val string_of_type : type_expr -> string
