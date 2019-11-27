4b2cd01baf4340dd45580cb248a3dc870c035cd1
Mukul Rathi <mukul.rathi@gmail.com>
2019-11-27 00:03:06 +0000

    Update docs for type-checker and env

--- src/typing/type_checker.mli
+++ src/typing/type_checker.mli
@@ -1,7 +1,11 @@
+(** The type checker takes a Parsed AST and type-checks the core language rules,
+    returning a typed AST if type-checking succeeds. It also optionally performs a second
+    stage of type-checking to check for data-race freedom. *)
+
 open Core
 
 val type_check_program :
-  Parsed_ast.program -> check_data_races:bool -> (Typed_ast.program, Error.t) Result.t
+  Parsed_ast.program -> check_data_races:bool -> Typed_ast.program Or_error.t
 
 val pprint_typed_ast : Format.formatter -> Typed_ast.program -> unit
 (** Given a formatter and typed AST, pretty-print the AST - useful for debugging *)
--- src/typing/type_env.mli
+++ src/typing/type_env.mli
@@ -1,3 +1,6 @@
+(** The type environment module consists of the definition of a type environment and
+    helper functions to operate on the environment *)
+
 open Ast_types
 open Core
 
@@ -5,16 +8,17 @@ type type_binding = Var_name.t * type_expr
 type type_env = type_binding list
 
 val check_type_equality : type_expr -> type_expr -> bool
+
 val field_to_expr_type : type_field -> type_expr
-val get_var_type : Var_name.t -> type_env -> loc -> (type_expr, Error.t) result
+(**Converts a field type to the equivalent expression type - used to check type equality *)
 
-val get_class_defn :
-  Class_name.t -> class_defn list -> loc -> (class_defn, Error.t) result
+(** A bunch of getter methods *)
 
-val get_class_field : Field_name.t -> class_defn -> loc -> (field_defn, Error.t) result
+val get_var_type : Var_name.t -> type_env -> loc -> type_expr Or_error.t
+val get_class_field : Field_name.t -> class_defn -> loc -> field_defn Or_error.t
 
 val get_obj_class_defn :
-  Var_name.t -> type_env -> class_defn list -> loc -> (class_defn, Error.t) result
+  Var_name.t -> type_env -> class_defn list -> loc -> class_defn Or_error.t
 
-val get_type_capability :
-  type_expr -> class_defn list -> loc -> (capability, Error.t) result
+val get_class_defn : Class_name.t -> class_defn list -> loc -> class_defn Or_error.t
+val get_type_capability : type_expr -> class_defn list -> loc -> capability Or_error.t
