f7a603b84217a9d06448ffac782b8cc221c7be56
Mukul Rathi <mukul.rathi@gmail.com>
2019-12-30 15:19:45 +0000

    TEMP: Remove data-race type-checker

--- src/typing/data_race/type_data_race.ml
+++
@@ -1,7 +0,0 @@
-open Core
-open Type_linear_ownership
-open Type_async_expr
-open Result
-
-let type_data_race program =
-  type_program_async_exprs program >>= fun () -> type_linear_ownership program
--- src/typing/data_race/type_data_race.mli
+++
@@ -1,7 +0,0 @@
-(** This module takes in a typed AST and then performs a second stage of type-checking,
-    only this time it is type-checking references for data-race freedom *)
-
-open Core
-open Typing_core_lang
-
-val type_data_race : Typed_ast.program -> unit Or_error.t
