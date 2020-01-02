e10f467cf29a6ee7125b44089cd40d3a4df84c63
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-02 19:29:16 +0000

    Update definition of desugared AST
    Resolves #68

--- src/desugaring/desugared_ast.ml
+++ src/desugaring/desugared_ast.ml
@@ -1,7 +1,9 @@
 (** The desugared AST consists of the typed AST but simplified.
 
     - for loops have been desugared into while loops.
-    - Shadowing is not permitted, so variables have been given unique monikers *)
+    - Shadowing is not permitted, so variables have been given unique monikers
+    - Block expressions have just been replaced by an expr list, since no shadowing so
+      block scope not necessary *)
 
 open Ast.Ast_types
 
@@ -15,19 +17,18 @@ type expr =
   | Integer     of loc * int  (** no need for type_expr annotation as obviously TEInt *)
   | Boolean     of loc * bool  (** no need for type_expr annotation as obviously TEBool *)
   | Identifier  of loc * identifier  (** Type information associated with identifier *)
-  | Block       of loc * type_expr * expr list  (** type is of the final expr in block *)
   | Constructor of loc * type_expr * Class_name.t * constructor_arg list
   | Let         of loc * type_expr * Var_name.t * expr
   | Assign      of loc * type_expr * identifier * expr
   | Consume     of loc * identifier  (** Type is associated with the identifier *)
   | MethodApp   of loc * type_expr * Var_name.t * type_expr * Method_name.t * expr list
   | FunctionApp of loc * type_expr * Function_name.t * expr list
-  | FinishAsync of loc * type_expr * expr list * expr
+  | FinishAsync of loc * type_expr * expr list list * expr list
       (** overall type is that of the expr on the current thread - since forked exprs'
           values are ignored *)
-  | If          of loc * type_expr * expr * expr * expr
+  | If          of loc * type_expr * expr * expr list * expr list
       (** If ___ then ___ else ___ - type is that of the branch exprs *)
-  | While       of loc * expr * expr
+  | While       of loc * expr * expr list
       (** While ___ do ___ ; - no need for type_expr annotation as type of a loop is
           TEVoid *)
   | BinOp       of loc * type_expr * bin_op * expr * expr
@@ -36,13 +37,13 @@ type expr =
 and constructor_arg = ConstructorArg of type_expr * Field_name.t * expr
 
 (** Function defn consists of the function name, return type, the list of params, and the
-    body expr of the function *)
-type function_defn = TFunction of Function_name.t * type_expr * param list * expr
+    body expr block of the function *)
+type function_defn = TFunction of Function_name.t * type_expr * param list * expr list
 
 (** Method defn consists the method name, return type, the list of params, the region
-    affected and the body expr of the function *)
+    affected and the body expr block of the function *)
 type method_defn =
-  | TMethod of Method_name.t * type_expr * param list * Region_name.t list * expr
+  | TMethod of Method_name.t * type_expr * param list * Region_name.t list * expr list
 
 (** Class definitions consist of the class name, its capability regions and the fields and
     methods in the class *)
@@ -50,5 +51,5 @@ type class_defn =
   | TClass of Class_name.t * region list * field_defn list * method_defn list
 
 (** Each bolt program defines the classes,followed by functions, followed by the main
-    expression to execute. *)
-type program = Prog of class_defn list * function_defn list * expr
+    expression block to execute. *)
+type program = Prog of class_defn list * function_defn list * expr list
