f14b4dbaa6f4bb755cf70317362e79b7a8964095
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-08 21:51:07 +0000

    Add backend to run program script

--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-make build
+make pre-build
 if [ $# -eq 0 ]; then
  dune exec -- src/frontend/main.exe -help
 else
   dune exec -- src/frontend/main.exe $*
+  bazel-bin/src/llvm-backend/main/main "${1%.bolt}.compiled"
 fi
- bazel-bin/src/llvm-backend/main/main
