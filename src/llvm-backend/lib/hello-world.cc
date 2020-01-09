c91b49a5f26474aded6c9d0c0305d1752939284a
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-09 10:17:24 +0000

    Remove stubs for test suite

--- src/llvm-backend/lib/hello-world.cc
+++
@@ -1,6 +0,0 @@
-#include "src/llvm-backend/lib/hello-world.h"
-
-#include <fstream>
-#include <string>
-
-std::string get_hello_world() { return ""; }
--- tests/llvm-backend/test_hello_world.cc
+++
@@ -1,4 +0,0 @@
-#include "gtest/gtest.h"
-#include "src/llvm-backend/lib/hello-world.h"
-
-TEST(TestHelloWorld, GetHelloWorld) { EXPECT_EQ(get_hello_world(), ""); }
