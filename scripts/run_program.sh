bbb563c1a8a7e8a736793b3c4ede31df23b27b13
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-24 16:16:57 +0000

    Update run-program script to compile assembly and execute

--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -4,5 +4,8 @@ if [ $# -eq 0 ]; then
  dune exec -- src/frontend/main.exe -help
 else
   dune exec -- src/frontend/main.exe $*
-  bazel-bin/src/llvm-backend/main/main "${1%.bolt}.compiled" > "${1%.bolt}.ll"
+  bazel-bin/src/llvm-backend/main "${1%.bolt}.compiled" > "${1%.bolt}.ll"
+  llc "${1%.bolt}.ll"
+  clang "${1%.bolt}.s" -o "${1%.bolt}"
+  ./${1%.bolt}
 fi
