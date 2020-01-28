f51b36818a5f4fbbef3e960e247c215baee78d30
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-28 00:54:42 +0000

    Don't try to run codegen if frontend returns error

--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -3,8 +3,10 @@ make pre-build
 if [ $# -eq 0 ]; then
  dune exec -- src/frontend/main.exe -help
 else
-  dune exec -- src/frontend/main.exe $*
-  bazel-bin/src/llvm-backend/main "${1%.bolt}.ir" > "${1%.bolt}.ll"
-  clang -pthread "${1%.bolt}.ll" -o "${1%.bolt}"
-  ./${1%.bolt}
+  (dune exec -- src/frontend/main.exe $*)
+  if [ -f "${1%.bolt}.ir" ]; then
+    bazel-bin/src/llvm-backend/main "${1%.bolt}.ir" > "${1%.bolt}.ll"
+    clang -pthread "${1%.bolt}.ll" -o "${1%.bolt}"
+    ./${1%.bolt}
+  fi
 fi
