349449472055e74c178411592f242e5666d27612
mukul-rathi <mukul.rathi@gmail.com>
2020-02-04 13:51:57 +0000

    Add function to update variable capabilities in-place

--- src/frontend/data_race_checker/data_race_checker_env.ml
+++ src/frontend/data_race_checker/data_race_checker_env.ml
@@ -43,3 +43,53 @@ let get_class_field_regions class_name field_name class_defns =
   List.filter
     ~f:(fun (TRegion (_, region_name)) -> elem_in_list region_name field_region_names)
     regions
+
+let update_var_capabilities_identifier var_name caps_update_fn id =
+  let open Data_race_checker_ast in
+  match id with
+  | Variable (_, name, _, caps) -> if var_name = name then caps_update_fn caps
+  | ObjField (_, obj_name, _, _, _, caps) ->
+      if var_name = obj_name then caps_update_fn caps
+
+let rec update_var_capabilities_expr var_name caps_update_fn expr =
+  let open Data_race_checker_ast in
+  let update_var_caps_expr_rec = update_var_capabilities_expr var_name caps_update_fn in
+  let update_var_caps_block_expr_rec =
+    update_var_capabilities_block_expr var_name caps_update_fn in
+  let update_var_caps_identifier_rec =
+    update_var_capabilities_identifier var_name caps_update_fn in
+  match expr with
+  | Integer _ | Boolean _ -> ()
+  | Identifier (_, id) -> update_var_caps_identifier_rec id
+  | BlockExpr (_, block_expr) -> update_var_caps_block_expr_rec block_expr
+  | Constructor (_, _, _, constructor_args) ->
+      List.iter
+        ~f:(fun (ConstructorArg (_, _, expr)) -> update_var_caps_expr_rec expr)
+        constructor_args
+  | Let (_, _, _, bound_expr) -> update_var_caps_expr_rec bound_expr
+  | Assign (_, _, id, assigned_expr) ->
+      update_var_caps_identifier_rec id ;
+      update_var_caps_expr_rec assigned_expr
+  | Consume (_, id) -> update_var_caps_identifier_rec id
+  | MethodApp (_, _, _, _, _, args) -> List.iter ~f:update_var_caps_expr_rec args
+  | FunctionApp (_, _, _, args) -> List.iter ~f:update_var_caps_expr_rec args
+  | Printf (_, _, args) -> List.iter ~f:update_var_caps_expr_rec args
+  | FinishAsync (_, _, async_exprs, _, curr_thread_expr) ->
+      List.iter
+        ~f:(fun (AsyncExpr (_, expr)) -> update_var_caps_block_expr_rec expr)
+        async_exprs ;
+      update_var_caps_block_expr_rec curr_thread_expr
+  | If (_, _, cond_expr, then_expr, else_expr) ->
+      update_var_caps_expr_rec cond_expr ;
+      update_var_caps_block_expr_rec then_expr ;
+      update_var_caps_block_expr_rec else_expr
+  | While (_, cond_expr, loop_expr) ->
+      update_var_caps_expr_rec cond_expr ;
+      update_var_caps_block_expr_rec loop_expr
+  | BinOp (_, _, _, expr1, expr2) ->
+      update_var_caps_expr_rec expr1 ;
+      update_var_caps_expr_rec expr2
+  | UnOp (_, _, _, expr) -> update_var_caps_expr_rec expr
+
+and update_var_capabilities_block_expr var_name caps_update_fn (Block (_, _, exprs)) =
+  List.iter ~f:(update_var_capabilities_expr var_name caps_update_fn) exprs
