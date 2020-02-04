dec3877ccb75c2e9c0b1b6c114cbee0fc364441f
mukul-rathi <mukul.rathi@gmail.com>
2020-02-04 17:36:01 +0000

    Update run program script to print when each stage complete

--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -6,8 +6,14 @@ if [ $# -eq 0 ]; then
 else
   (dune exec -- src/frontend/main.exe $*)
   if [ -f "${1%.bolt}.ir" ]; then
+    echo -e "Frontend type-checking completed \033[0;32msuccessfully".
+    echo -e "\033[0m"
     bazel-bin/src/llvm-backend/main "${1%.bolt}.ir" > "${1%.bolt}.ll"
+    echo -e "LLVM IR Codegen completed \033[0;32msuccessfully".
+    echo -e "\033[0m"
     clang -pthread "${1%.bolt}.ll" -o "${1%.bolt}"
+    echo  -e "Native binary compiled \033[0;32msuccessfully".
+     echo -e "\033[0m"
     ./${1%.bolt}
   fi
 fi
