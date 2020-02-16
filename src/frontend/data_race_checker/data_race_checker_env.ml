8b377696a825d4a5e1a0711a1f56e4e92b92f370
mukul-rathi <mukul.rathi@gmail.com>
2020-02-16 21:28:31 +0000

    Check if regions can be concurrently accessed
    Resolves #104

--- src/frontend/data_race_checker/data_race_checker_env.ml
+++ src/frontend/data_race_checker/data_race_checker_env.ml
@@ -2,13 +2,15 @@ open Ast.Ast_types
 open Core
 open Desugaring.Desugared_ast
 
+let rec elem_in_list x = function [] -> false | y :: ys -> x = y || elem_in_list x ys
+let intersect_lists list1 list2 = List.filter ~f:(fun x -> elem_in_list x list2) list1
+
 let get_class_defn class_name class_defns =
   let matching_class_defns =
     List.filter ~f:(fun (TClass (name, _, _, _)) -> class_name = name) class_defns in
+  (* This should never throw an exception since we've checked this property in earlier
+     type-checking stages of the pipeline *)
   List.hd_exn matching_class_defns
-
-(* This should never throw an exception since we've checked this property in earlier
-   type-checking stages of the pipeline *)
 
 let get_class_regions class_name class_defns =
   get_class_defn class_name class_defns |> fun (TClass (_, regions, _, _)) -> regions
@@ -16,12 +18,26 @@ let get_class_regions class_name class_defns =
 let get_class_field field_name (TClass (_, _, field_defns, _)) =
   let matching_field_defns =
     List.filter ~f:(fun (TField (_, _, name, _)) -> field_name = name) field_defns in
+  (* This should never throw an exception since we've checked this property in earlier
+     type-checking stages of the pipeline *)
   List.hd_exn matching_field_defns
 
-(* This should never throw an exception since we've checked this property in earlier
-   type-checking stages of the pipeline *)
+let get_class_field_regions class_name field_name class_defns =
+  get_class_defn class_name class_defns
+  |> fun (TClass (_, regions, _, _) as class_defn) ->
+  get_class_field field_name class_defn
+  |> fun (TField (_, _, _, field_region_names)) ->
+  List.filter
+    ~f:(fun (TRegion (_, region_name)) -> elem_in_list region_name field_region_names)
+    regions
 
-let rec elem_in_list x = function [] -> false | y :: ys -> x = y || elem_in_list x ys
+let get_class_region_fields class_name region_name class_defns =
+  get_class_defn class_name class_defns
+  |> fun (TClass (_, _, fields, _)) ->
+  List.filter
+    ~f:(fun (TField (_, _, _, field_region_names)) ->
+      elem_in_list region_name field_region_names)
+    fields
 
 let class_has_capability class_name cap class_defns =
   let rec class_has_capability_helper class_name cap class_defns seen_class_names =
@@ -56,14 +72,70 @@ let class_has_capability class_name cap class_defns =
   in
   class_has_capability_helper class_name cap class_defns []
 
-let get_class_field_regions class_name field_name class_defns =
-  get_class_defn class_name class_defns
-  |> fun (TClass (_, regions, _, _) as class_defn) ->
-  get_class_field field_name class_defn
-  |> fun (TField (_, _, _, field_region_names)) ->
-  List.filter
-    ~f:(fun (TRegion (_, region_name)) -> elem_in_list region_name field_region_names)
-    regions
+let type_has_capability type_expr cap class_defns =
+  match type_expr with
+  | TEClass (class_name, _) -> class_has_capability class_name cap class_defns
+  | _                       -> false
+
+let region_fields_have_capability (TRegion (region_cap, region_name)) class_name cap
+    class_defns =
+  region_cap = cap
+  || get_class_region_fields class_name region_name class_defns
+     |> fun fields_in_region ->
+     List.exists
+       ~f:(fun (TField (_, field_type, _, _)) ->
+         match field_type with
+         | TEClass (field_class, _) -> class_has_capability field_class cap class_defns
+         | _                        -> false)
+       fields_in_region
+
+(* There is another region in the class that can both access subordinate state in one
+   region and also subordinate state in another region - thus acting as a channel for
+   them. *)
+let regions_have_subord_channel class_name class_defns region_1_name region_2_name =
+  let get_reg_subord_fields region_name =
+    List.filter
+      ~f:(fun (TField (_, field_type, _, _)) ->
+        type_has_capability field_type Subordinate class_defns)
+      (get_class_region_fields class_name region_name class_defns) in
+  (* collect the regions that aren't region 1 or region 2 and have access to subord state*)
+  let get_potential_channel_regions sub_ord_fields =
+    List.concat_map
+      ~f:(fun (TField (_, _, _, field_reg_names)) ->
+        List.filter
+          ~f:(fun reg_name ->
+            (not (reg_name = region_1_name)) && not (reg_name = region_2_name))
+          field_reg_names)
+      sub_ord_fields in
+  let get_region_1_potential_channels =
+    get_potential_channel_regions (get_reg_subord_fields region_1_name) in
+  let get_region_2_potential_channels =
+    get_potential_channel_regions (get_reg_subord_fields region_1_name) in
+  (* check if a region in intersection of these potential channels *)
+  let subord_channels =
+    intersect_lists get_region_1_potential_channels get_region_2_potential_channels in
+  List.length subord_channels > 0
+
+(* Check that overlapping fields have safe capability and are not subordinate *)
+let regions_have_safe_unsubord_shared_state class_name class_defns region_1_name
+    region_2_name =
+  let region_1_fields = get_class_region_fields class_name region_1_name class_defns in
+  let region_2_fields = get_class_region_fields class_name region_2_name class_defns in
+  let shared_fields = intersect_lists region_1_fields region_2_fields in
+  List.for_all
+    ~f:(fun (TField (_, field_type, _, _)) ->
+      (not (type_has_capability field_type Subordinate class_defns))
+      && type_has_capability field_type Safe class_defns)
+    shared_fields
+
+let can_concurrently_access_regions class_name class_defns
+    (TRegion (region_1_cap, region_1_name)) (TRegion (_, region_2_name)) =
+  regions_have_safe_unsubord_shared_state class_name class_defns region_1_name
+    region_2_name
+  && (not
+        (regions_have_subord_channel class_name class_defns region_1_name region_2_name))
+  (* Can't access the same linear region in multiple threads as violates linearity *)
+  && not (region_1_cap = Linear && region_1_name = region_2_name)
 
 let rec reduce_expr_to_obj_id expr =
   match expr with
--- src/frontend/data_race_checker/data_race_checker_env.mli
+++ src/frontend/data_race_checker/data_race_checker_env.mli
@@ -4,14 +4,22 @@ open Ast.Ast_types
 open Desugaring.Desugared_ast
 
 val class_has_capability : Class_name.t -> capability -> class_defn list -> bool
-(** Check if a class has a given capability *)
+
+val region_fields_have_capability :
+  region -> Class_name.t -> capability -> class_defn list -> bool
 
 val get_class_regions : Class_name.t -> class_defn list -> region list
 
 val get_class_field_regions :
   Class_name.t -> Field_name.t -> class_defn list -> region list
 
+val get_class_region_fields :
+  Class_name.t -> Region_name.t -> class_defn list -> field_defn list
+
 val elem_in_list : 'a -> 'a list -> bool
+
+val can_concurrently_access_regions :
+  Class_name.t -> class_defn list -> region -> region -> bool
 
 val reduce_expr_to_obj_id : expr -> identifier list
 (** Use abstract interpretation to return the potential object identifiers this expression
