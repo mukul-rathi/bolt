160670be0fee1548b0c3db52cffa5c271672948a
mukul-rathi <mukul.rathi@gmail.com>
2020-02-17 11:19:38 +0000

    Add helper function to update identifier regions

---
+++ src/frontend/data_race_checker/update_identifier_regions.mli
@@ -0,0 +1,12 @@
+open Ast.Ast_types
+open Desugaring.Desugared_ast
+
+val update_identifier_regions_expr :
+  Var_name.t -> (region list -> region -> bool) -> expr -> expr
+(** Update all identifiers that match the given name,filtering by
+    [region list -> region -> bool] - a function that given a list of all the regions,
+    decides whether a region should be in the identifier. We also propagate the update to
+    any aliases made of the identifier. (note this is not a symmetric propagation) *)
+
+val update_identifier_regions_block_expr :
+  Var_name.t -> (region list -> region -> bool) -> block_expr -> block_expr
