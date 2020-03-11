c7b7aeaa22741355b6cf54358c60df4c83f90738
mukul-rathi <mukul.rathi@gmail.com>
2020-03-11 13:30:25 +0000

    Fix expect tests for data-race typechecker

--- tests/frontend/expect/data_race_checker/print_data_race_checker_ast.ml
+++ tests/frontend/expect/data_race_checker/print_data_race_checker_ast.ml
@@ -1,4 +1,4 @@
 open Compile_program_ir
 
 let print_data_race_checker_ast input_str =
-  compile_program_ir (Lexing.from_string input_str) ~should_pprint_dast:true
+  compile_program_ir (Lexing.from_string input_str) ~should_pprint_drast:true
