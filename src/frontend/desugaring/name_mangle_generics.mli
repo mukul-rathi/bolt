831eb2d21395d201d0f552d5c714ed5b921d8577
mukul-rathi <mukul.rathi@gmail.com>
2020-04-28 01:36:44 +0100

    Refactor generics desugaring into separate sub-modules

--- src/frontend/desugaring/name_mangle_generics.mli
+++ src/frontend/desugaring/name_mangle_generics.mli
@@ -1,7 +1,7 @@
-open Typing
 (** This module is responsible for name-mangling generics, so Foo<int> -> _Fooint *)
 
 open Ast.Ast_types
+open Typing
 
 val name_mangle_generic_class : Class_name.t -> type_expr -> Class_name.t
 val name_mangle_if_generic_class : Class_name.t -> type_expr option -> Class_name.t
