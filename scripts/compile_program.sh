98df7e357a4889f199e8a96b5a45de62fbcc0d6f
mukul-rathi <mukul.rathi@gmail.com>
2020-06-12 11:38:13 +0100

    Update boltc compile script

--- scripts/compile_program.sh
+++ scripts/compile_program.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-boltc () {
 make pre-build
 rm -rf "${1%.bolt}.ir"
 if [ $# -eq 0 ]; then
@@ -11,8 +10,7 @@ else
     echo -e "\033[0m"
     bazel-bin/src/llvm-backend/main "${1%.bolt}.ir" > "${1%.bolt}.ll" && echo -e "LLVM IR Codegen completed \033[0;32msuccessfully".
     echo -e "\033[0m"
-    clang -pthread "${1%.bolt}.ll" -o "${1%.bolt}" && echo  -e "Native binary compiled \033[0;32msuccessfully".
+    clang -O3 -pthread "${1%.bolt}.ll" -o "${1%.bolt}" && echo  -e "Native binary compiled \033[0;32msuccessfully".
      echo -e "\033[0m"
   fi
 fi
-}
--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-make pre-build
-rm -rf "${1%.bolt}.ir"
-if [ $# -eq 0 ]; then
- dune exec -- src/frontend/main.exe -help
-else
-  (dune exec -- src/frontend/main.exe $*)
-  if [ -f "${1%.bolt}.ir" ]; then
-    echo -e "Frontend type-checking completed \033[0;32msuccessfully".
-    echo -e "\033[0m"
-    bazel-bin/src/llvm-backend/main "${1%.bolt}.ir" > "${1%.bolt}.ll" && echo -e "LLVM IR Codegen completed \033[0;32msuccessfully".
-    echo -e "\033[0m"
-    clang -O3 -pthread "${1%.bolt}.ll" -o "${1%.bolt}" && echo  -e "Native binary compiled \033[0;32msuccessfully".
-     echo -e "\033[0m"
-    ./${1%.bolt}
-  fi
+scripts/compile_program.sh $* &&  if [ -f "${1%.bolt}.ir" ]; then ./${1%.bolt} 
 fi
